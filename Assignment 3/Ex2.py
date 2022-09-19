d631ff82bf3c1701da2436a0ce4106d2c91642da
Eswar <109469319+eswarravi317@users.noreply.github.com>
2022-09-19 21:25:21 +0530

    Delete Assignment 3 directory

--- Assignment 3/Ex2.py
+++
@@ -1,8 +0,0 @@
-maximum = int(input(" Please Enter the Maximum Value : "))
-
-number = 1
-
-while number <= maximum:
-    if(number % 2 != 0):
-        print("{0}".format(number))
-    number = number + 1
